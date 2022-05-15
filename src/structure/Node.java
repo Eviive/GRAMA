3269e841b3885fab9b202c7259cd4bf77c69c553
Eviive <albert.vaillon21@gmail.com>
2022-05-15 16:43:44 +0200

    Changed the way getNeighbors() works

--- src/structure/Node.java
+++ src/structure/Node.java
@@ -5,20 +5,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * The class representing a Node
+ * The class representing a node
  * @author VAILLON Albert
+ * @version JDK 11.0.13
  */
-public class Node {
+public final class Node {
 	
 	private final NodeType type;
 	private final String name;
-	private List<Link> linkList = new ArrayList<>();
+	private final List<Link> linkList = new ArrayList<>();
 	
 	/**
-	 * Creates a new Node
-	 * @param type The type of the Node
-	 * @param name The name of the Node
-	 * @throws LoadGraphException If <code>NodeType.typeOf(type)</code> throws a LoadGraphException
+	 * Creates a new <code>Node</code>
+	 * @param type The type of the <code>Node</code>
+	 * @param name The name of the <code>Node</code>
+	 * @throws LoadGraphException If <code>NodeType.typeOf(type)</code> throws a <code>LoadGraphException</code>
 	 */
 	public Node(char type, String name) throws LoadGraphException {
 		this.type = NodeType.typeOf(type);
@@ -26,70 +27,109 @@ public class Node {
 	}
 	
 	/**
-	 * @return Returns the type of the Node
+	 * @return Returns the type of this <code>Node</code>
 	 */
 	public NodeType getType() {
 		return type;
 	}
 	
 	/**
-	 * @return Returns the name of the Node
+	 * @return Returns the name of this <code>Node</code>
 	 */
 	public String getName() {
 		return name;
 	}
 	
 	/**
-	 * @return Returns the list of the Node's links
+	 * @return Returns the <code>List</code> of this <code>Node</code>'s <code>Links</code>
 	 */
 	public List<Link> getNodeLinks() {
 		return linkList;
 	}
 	
 	/**
-	 * @return Returns the list of the Node's direct neighbors
+	 * @param nbJumps The number of jumps we have to do
+	 * @param results The <code>List</code> in which the results will go
+	 * @return Returns the <code>List</code> of all the <code>Nodes</code> you can go to by making <code>nbJumps</code> jumps or less from this <code>Node</code>
 	 */
-	public List<Node> getNeighbors() {
-		return linkList.stream()
-					   .map(link -> link.getDestination())
-					   .collect(Collectors.toList());
+	public List<Node> getNeighbors(int nbJumps, List<Node> results) {
+		--nbJumps;
+		
+		// if nbJumps is less than 0 you can't go anywhere but where you are
+		if (nbJumps < 0) {
+			results.add(this);
+			return results;
+		}
+		
+		// the List of all the neighbors of this Node
+		List<Node> neighbors = linkList.stream()
+									   .map(link -> link.getDestination())
+									   .collect(Collectors.toList());
+		
+		// if it's not the last jump (nbJumps == 0) then do getNeighbors() on all of them
+		if (nbJumps > 0) {
+			for (Node neighbor: neighbors) {
+				neighbor.getNeighbors(nbJumps, results)
+						.stream()
+						.filter(node -> !results.contains(node))
+						.forEach(node -> results.add(node));
+			}
+			return results;
+		// if it's the last jump (nbJumps == 0) then we add the neighbors to the results List if they're not already in it
+		} else {
+			neighbors.stream()
+					 .filter(node -> !results.contains(node))
+					 .forEach(node -> results.add(node));
+			return results;
+		}
 	}
 	
 	/**
-	 * @param type The type of the Nodes we want to get
-	 * @return Returns the list of the Node's direct neighbors with the right type
+	 * @param nbJumps The number of jumps we have to do
+	 * @param results The <code>List</code> in which the results will go
+	 * @param type The type of the <code>Nodes</code> we want to get
+	 * @return Returns the <code>List</code> of all the <code>Nodes</code> of type <code>type</code> you can go to by making <code>nbJumps</code> jumps or less from this <code>Node</code>
 	 */
-	public List<Node> getNeighbors(NodeType type) {
-		return linkList.stream()
-					   .map(link -> link.getDestination())
-					   .filter(node -> node.getType() == type)
-					   .collect(Collectors.toList());
+	public List<Node> getNeighbors(int nbJumps, List<Node> results, NodeType type) {
+		return getNeighbors(nbJumps, results).stream()
+											 .filter(node -> node.getType() == type)
+											 .collect(Collectors.toList());
 	}
 	
 	/**
-	 * Adds a new link to the Node
-	 * @param link The link that will be added
+	 * Adds a new <code>Link</code> to the <code>Node</code>
+	 * @param link The <code>Link</code> that will be added
 	 */
 	public void addLink(Link link) {
 		linkList.add(link);
 	}
 	
 	/**
-	 * Tells us if two Nodes are at two distances of each other or not
-	 * @param target The targeted Node
-	 * @return Returns <code>true</code> if the Nodes are at two distance from each other, else <code>false</code>
+	 * Tells us if this <code>Node</code> is at two distances from the target <code>Node</code>
+	 * @param target The targeted <code>Node</code>
+	 * @return Returns <code>true</code> if the <code>Nodes</code> are at two distance from each other
 	 */
 	public boolean isTwoDistance(Node target) {
-		for (Node oneDistanceNeighbors: getNeighbors()) {
-			for (Node twoDistanceNeighbors: oneDistanceNeighbors.getNeighbors()) {
-				if (twoDistanceNeighbors.equals(target)) {
-					return true;
-				}
-			}
-		}
-		return false;
+		return getNeighbors(2, new ArrayList<>()).contains(target);
 	}
 	
+	/**
+	 * Tells us if this <code>Node</code> has more two jumps neighbors of type <code>type</code> than the <code>target Node</code>
+	 * @param target The targeted <code>Node</code>
+	 * @param type The type of <code>Nodes</code> we're counting
+	 * @return Returns <code>true</code> if this <code>Node</code> has the same number of two jumps neighbors of type <code>type</code> than the <code>target Node</code> or more
+	 */
+	public boolean isMoreLinkedToType(Node target, NodeType type) {
+		int nbNodeCities = getNeighbors(2, new ArrayList<>(), type).size();
+		int nbTargetCities = target.getNeighbors(2, new ArrayList<>(), type).size();
+		return nbNodeCities >= nbTargetCities;
+	}
+	
+	/**
+	 * Compares this <code>Node</code> to the specified <code>Object</code>
+	 * @param o The <code>Object</code> we want to compare to this <code>Node</code>
+	 * @return Returns <code>true</code> if this <code>Node</code> and the <code>Object o</code> are the same
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (!(o instanceof Node)) {
@@ -99,6 +139,10 @@ public class Node {
 		return name.equals(p.name) && type == p.type;
 	}
 	
+	/**
+	 * Creates a <code>String</code> object representing this <code>Node</code>
+	 * @return Returns a <code>String</code> representation of this <code>Node</code>
+	 */
 	@Override
 	public String toString() {
 		return type + " | " + name;
--- src/test/TestNode.java
+++ src/test/TestNode.java
@@ -1,22 +1,31 @@
 package test;
 
+import java.util.ArrayList;
 import structure.Graph;
 import structure.LoadGraphException;
 import structure.Node;
 import structure.NodeType;
 
+/**
+ * Test class for Node
+ * @author VAILLON Albert
+ * @version JDK 11.0.13
+ */
 public class TestNode {
 	
 	public static void main(String[] args) throws LoadGraphException {
-		Graph graph = new Graph("graph.csv");
+		Graph graph = new Graph();
+		graph.load("graph.csv");
 		
-		Node chanas = graph.getNodeList().get("Chanas");
+		Node chanas = graph.getNodeMap().get("Chanas");
 		if (chanas != null) {
-			System.out.println("Chanas' neighbors : " + chanas.getNeighbors() + "\n");
-			System.out.println("Chanas' restaurant neighbors : " + chanas.getNeighbors(NodeType.RESTAURANT) + "\n");
+			System.out.println("Chanas' neighbors : " + chanas.getNeighbors(1, new ArrayList<>()) + "\n");
+			System.out.println("Chanas' restaurant neighbors : " + chanas.getNeighbors(2, new ArrayList<>(), NodeType.RESTAURANT) + "\n");
 			
-			Node lyon = graph.getNodeList().get("Lyon");
+			Node lyon = graph.getNodeMap().get("Lyon");
 			System.out.println("Lyon is at two jumps from Chanas : " + lyon.isTwoDistance(chanas));
+			
+			System.out.println("Lyon is connected to more two jump cities : " + lyon.isMoreLinkedToType(chanas, NodeType.CITY));
 		}
 	}
 	
